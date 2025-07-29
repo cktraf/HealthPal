b003c4234692b2964a79286eb8bbb9df2b3cba4a
Carissa Traffie <carissa@traffie.com>
2025-07-29 10:45:46 -0700

    add basic button

--- PhysicalHealthView.swift
+++ PhysicalHealthView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct PhysicalHealthView: View {
     var body: some View {
-        Text("Physical Health")
+        VStack(alignment: .leading) {
+            Text("Welcome, <name>!")
+                .font(.title)
+                .multilineTextAlignment(.leading)
+            Text("Log Activity:")
+                .font(.title)
+                .multilineTextAlignment(.leading)
+            Text("Activity Time (minutes):")
+                .font(.title3)
+            HStack {
+                Button("10") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .background(Color.blue)
+                .cornerRadius(7)
+            }
+        }
     }
 }
 
